45ff35effb523e322656044e775d3b578c9dff61
三村友幾 <yuki@yuki.local>
2021-02-19 23:41:06 +0900

    capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+# config valid for current version and patch releases of Capistrano
+lock '3.15.0'
+
+# Capistranoのログの表示に利用する
+set :application, 'hoge'
+set :deploy_to, '/var/www/hoge'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:yukimimura/hoge.git'
+set :branch, 'main'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.6' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/hoge.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
